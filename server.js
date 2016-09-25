3ddafd18dd0c0de58c1e11a2d43db1434b1181c1
Someshagrawal <someshagra01@gmail.com>
2016-09-25 12:50:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
+app.get('/ui/20150928_220044', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', '20150928_220044.jpg'));
 });
 
