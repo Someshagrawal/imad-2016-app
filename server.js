09fd963bf19cb0e38b79989df9406080615f4967
Someshagrawal <someshagra01@gmail.com>
2016-10-15 09:49:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+ counter=0;
+ app.get('/counter',function (req,res) {
+counter=counter+1;
+res.send(counter.toString());
+ });
 app.get('/New2',function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'New2.html'));
 });
