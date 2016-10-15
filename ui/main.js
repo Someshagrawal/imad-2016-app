ff8ce4b90ec586a373b453cd5d788200f166196c
Someshagrawal <someshagra01@gmail.com>
2016-10-15 10:19:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,8 @@
-console.log('Loaded!');
-var ele=document.getElementById('madi');
-mRight= 0;
-function move(){
-  mRight= mRight + 10;  
-  ele.style.marginRight= mRight + 'px';
-}
-ele.onclick = function(){
-var interval= ele.setInteval(move,10);
-};
 
+counter=0;
 var button=document.getElementById('counter');
 button.onClick = function() {
-    counter=0;
+    
     counter=counter+1;
     var span=document.getElementById('count');
     span.innerHTML = counter.toString();
