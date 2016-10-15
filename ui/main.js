064807906eb8fadef627376a46b776803756242a
Someshagrawal <someshagra01@gmail.com>
2016-10-15 10:15:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,3 +8,12 @@ function move(){
 ele.onclick = function(){
 var interval= ele.setInteval(move,10);
 };
+
+var button=document.getElementById('counter');
+button.onClick = function() {
+    counter=0;
+    counter=counter+1;
+    var span=document.getElementById('count');
+    span.innerHTML = counter.toString();
+    
+};
