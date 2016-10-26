8c6c30870eb8f14ffdc208cddbecd318cbf9118a
Someshagrawal <someshagra01@gmail.com>
2016-10-26 21:56:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/', function (req, res) {
 
 var pool = new Pool(config);
 app.get('/test-db',function(req, res){
-    pool.query('Select * from book',function(err, result){
+    pool.query('Select * from test',function(err, result){
         if(err)
         res.status(500).send(res.toString());
         else 
