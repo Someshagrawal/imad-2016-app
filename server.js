408cf7b323fe4c3201e53941f486af209812edf4
Someshagrawal <someshagra01@gmail.com>
2016-10-27 22:40:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ app.get('/:articlename', function (req, res) {
 
 app.get('/articles/:articleName', function (req, res) {
     //articleName == Article-one
-    pool.query('SELECT * FROM article where title=`' + req.params.articleName+'`', function (err, result) {
+    pool.query('SELECT * FROM article where title=' + req.params.articleName, function (err, result) {
     if(err)
     res.status(500).send(err.toString());
     else{
