db9620d94957d31791fd1e33992cf73c6011a20b
Someshagrawal <someshagra01@gmail.com>
2016-11-01 10:30:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,9 @@
 var counter = 0;
-var button = document.getElementById('counter');
+var button = document.getElementById(''counter');
+document.write("Somesh");
 button.onClick = function() {
     
     counter = counter + 1;
     var span = document.getElementById('count');
     span.innerHTML = counter.toString();
-    
 };
