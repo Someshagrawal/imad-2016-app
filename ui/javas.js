5b25e01d8db02c5cda0d1c7cb201c25cfd56fceb
Someshagrawal <someshagra01@gmail.com>
2016-11-01 10:35:22 +0530

    [imad-console] Updates ui/javas.js

--- ui/javas.js
+++ ui/javas.js
@@ -1,7 +1,6 @@
 var counter = 0;
 var button = document.getElementById('counter');
 button.onClick = function() {
-    
     counter = counter + 1;
     var span = document.getElementById('count');
     span.innerHTML = counter.toString();
