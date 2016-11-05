2bbfac813c6b3b67d22affcab73afdb56803a1c0
Someshagrawal <someshagra01@gmail.com>
2016-11-05 20:02:28 +0530

    Delete javas.js

--- ui/javas.js
+++
@@ -1,7 +0,0 @@
-var counter = 0;
-var button = document.getElementById('counter');
-button.onClick = function() {
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
-};
