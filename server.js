55a911dc8c46c99d61e29ac5bb1d271656a1885e
Someshagrawal <someshagra01@gmail.com>
2016-11-07 13:26:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.post('/sign-up', function (req, res) {
   var password = 'open';
    var salt = crypto.randomBytes(128).toString('hex');
    var dbString = hash(password, salt);
-   pool.query("INSERT INTO users VALUES ('"+username+"','"+ dbString+"')", function (err, result) {
+   pool.query("INSERT INTO users VALUES ('"+username+"','"+ dbString+"');", function (err, result) {
       if (err) {
           res.status(500).send(err.toString());
       } else {
