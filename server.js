65e8d0c713d7117afff46af9ab30ae0e3ab03760
Someshagrawal <someshagra01@gmail.com>
2016-11-08 14:49:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -147,10 +147,22 @@ app.get('/ui/login.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'login.jpg'));
 });
 app.get('/ui/signup.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'login.jpg'));
+  res.sendFile(path.join(__dirname, 'ui', 'signup.jpg'));
 });
 app.get('/ui/signup.html', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'login.jpg'));
+  res.sendFile(path.join(__dirname, 'ui', 'signup.html'));
+});
+app.get('/ui/blogpot.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'blogpot.jpg'));
+});
+app.get('/blog/:input', function (req, res) {
+  var topic = req.params.input;
+  res.send(topic);
+  
+  
+  
+  
+  
 });
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
