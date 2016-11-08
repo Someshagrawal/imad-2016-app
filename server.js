a8e5cbc6c06aca79e28837a12596f7cac914fc3c
Someshagrawal <someshagra01@gmail.com>
2016-11-08 13:54:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -45,7 +45,8 @@ app.post('/signup', function (req, res) {
       if (err) {
           res.status(500).send(err.toString());
       } else {
-          res.send('User successfully created: ' + username);
+        alert('User successfully created: ' + username);
+        res.sendFile(path.join(__dirname, 'ui', 'login.html'));
       }
    });
 });
@@ -66,7 +67,6 @@ app.post('/login', function (req, res) {
               var hashedPassword = hash(password, salt);
               if (hashedPassword === dbString) {
                 req.session.auth = {userId: result.rows[0].id};
-    
                 res.send('credentials correct!');
                 
               } else {
@@ -79,7 +79,6 @@ app.post('/login', function (req, res) {
 
 app.get('/check-login', function (req, res) {
    if (req.session && req.session.auth && req.session.auth.userId) {
-       // Load the user object
        pool.query('SELECT * FROM "user" WHERE id = $1', [req.session.auth.userId], function (err, result) {
            if (err) {
               res.status(500).send(err.toString());
