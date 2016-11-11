68a39ab7ec6f29299f0efd9aaa36036a47f2c1a0
Someshagrawal <someshagra01@gmail.com>
2016-11-11 10:27:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -168,12 +168,12 @@ app.get('/blog/:input', function (req, res) {
            } 
        else{
      var blogfull = '<!DOCTYPE html><html><head><title>'+ topic +'</title></head><body><h1 align="center">'+ topic +'</h1>';
-     for(i=0;i<result.rows.length;i++){
-  blogfull = blogfull + '<br><br><br> <h2>' + result.rows[0].Title + '</h2><p>' + result.rows[0].Article +
-     '</p><h3>Comments</h3>' + result.rows[0].comment + '<h4>Your Comment:</h4><input id="T'+result.rows[0].Sno+'"  type="text"/><button id="B'+ result.rows[0].Sno+'">SUBMIT</button>';
+     for(i=0;i<result.rows.length;i=i+1){
+  blogfull = blogfull + '<br><br><br> <h2>' + result.rows[i].Title + '</h2><p>' + result.rows[i].Article +
+     '</p><h3>Comments</h3>' + result.rows[i].comment + '<h4>Your Comment:</h4><input id="T'+result.rows[i].Sno+'"  type="text"/><button id="B'+ result.rows[i].Sno+'">SUBMIT</button>';
      }
-blogfull =  blogfull+ '</body><script>';  
-res.send(blogfull);
+        blogfull =  blogfull + '</body><script>';  
+        res.send(blogfull);
     
        }
        });
